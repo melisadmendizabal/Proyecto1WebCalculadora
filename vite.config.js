77607ac79c3a794dae78196ad13601fa83d96c1e
Melisa <melisadmendizabal@gmail.com>
2025-06-03 02:15:56 -0600

    Cambios en el gitignore

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Melisa/CalcKuromi/',
   test: {
     environment: 'jsdom',
     globals: true,
